636e5a7a95ebac0c35d21cbf96ca3aab855af86c
modelable <62459471+modelable@users.noreply.github.com>
2024-06-05 19:51:03 +0900

    commit

--- src/main/java/kr/ac/hansung/cse/hellospringbootsecurity/entity/MyUser.java
+++ src/main/java/kr/ac/hansung/cse/hellospringbootsecurity/entity/MyUser.java
@@ -1,2 +1,35 @@
-package kr.ac.hansung.cse.hellospringbootsecurity.entity;public class MyUser {
+package kr.ac.hansung.cse.hellospringbootsecurity.entity;
+
+// 'User'는 스프링에 존재하는 클래스 이름이므로 클래스명을 'MyUser'로 설정
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name="users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class MyUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @ManyToMany(cascade = CascadeType.MERGE)
+    @JoinTable(
+            name="user_role",
+            joinColumns = {@JoinColumn(name="USER_ID", referencedColumnName = "ID")},
+            inverseJoinColumns = {@JoinColumn(name="ROLE_ID", referencedColumnName = "ID")}
+    )
+    private List<MyRole> roles;
 }
--- src/main/java/kr/ac/hansung/cse/hellospringbootsecurity/repository/RoleRepository.java
+++ src/main/java/kr/ac/hansung/cse/hellospringbootsecurity/repository/RoleRepository.java
@@ -1,2 +1,10 @@
-package kr.ac.hansung.cse.hellospringbootsecurity.repository;public class RoleRepository {
+package kr.ac.hansung.cse.hellospringbootsecurity.repository;
+
+import kr.ac.hansung.cse.hellospringbootsecurity.entity.MyRole;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<MyRole, Integer> {
+    Optional<MyRole> findByRolename(String rolename);
 }
